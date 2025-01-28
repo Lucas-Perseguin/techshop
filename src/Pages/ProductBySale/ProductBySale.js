23e778cac62c2a168080af04f11e2fe66fc81477
Lucas-Perseguin <lucasperseguin@gmail.com>
2025-01-28 11:25:24 -0300

    fix: paginação

--- src/Pages/ProductBySale/ProductBySale.js
+++ src/Pages/ProductBySale/ProductBySale.js
@@ -1,28 +1,34 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Container } from "./ProductBySale.style";
 import Products from "../../Components/Products/Products";
+import { Container } from "./ProductBySale.style";
 
 export default function ProductBySale() {
-  const [products, setProducts] = useState([]);
-  const params = useParams();
+	const [products, setProducts] = useState([]);
+	const [page, setPage] = useState(1);
+	const params = useParams();
 
-  useEffect(() => {
-    const promise = axios.get(
-      process.env.REACT_APP_BACK_END_API_URI +
-        "/products/" +
-        params.type +
-        "/sale"
-    );
-    promise.then(({ data }) => {
-      setProducts(data);
-    });
-  }, [params]);
+	useEffect(() => {
+		const promise = axios.get(
+			process.env.REACT_APP_BACK_END_API_URI +
+				"/products/" +
+				params.type +
+				"/sale",
+		);
+		promise.then(({ data }) => {
+			setProducts(data);
+		});
+	}, [params]);
 
-  return (
-    <Container>
-      <Products products={products} type={params.type + " em promoção"} />
-    </Container>
-  );
+	return (
+		<Container>
+			<Products
+				products={products}
+				type={params.type + " em promoção"}
+				page={page}
+				setPage={setPage}
+			/>
+		</Container>
+	);
 }
